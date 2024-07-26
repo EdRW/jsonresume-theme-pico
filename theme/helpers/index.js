c11a41e5dcef65e9ba516dcce9c1996fd9351aa3
Edmund Rosewright <edmundrosewright@gmail.com>
2024-07-26 23:44:19 +0900

    feat: add nested work section

--- theme/helpers/index.js
+++ theme/helpers/index.js
@@ -1,5 +1,7 @@
 import { dateHelpers } from './date-helpers.js';
+import { nestedWork } from './nested-work.js';
 
 export default {
   ...dateHelpers,
+  nestedWork,
 };
---
+++ theme/helpers/nested-work.js
@@ -0,0 +1,34 @@
+/**
+ * @typedef {Required<import('../../types.d.ts').ResumeSchema>['work']} WorkArray
+ * @typedef {import('../../types.d.ts').HelperOptions} Options
+ * @typedef {import('../../types.d.ts').NestedWork[]} NestedWorkArray
+ */
+/**
+ * @param {WorkArray} workArray
+ * @param {Options} options
+ * @returns {string}
+ */
+export function nestedWork(workArray, options) {
+  const nestedWork = workArray.reduce(
+    (acc, { location, description, name, url, ...rest }) => {
+      const prev = acc[acc.length - 1];
+      if (
+        prev &&
+        prev.name === name &&
+        prev.location === location &&
+        prev.description === description &&
+        prev.url === url
+      )
+        prev.items.push(rest);
+      else acc.push({ location, description, name, url, items: [rest] });
+      return acc;
+    },
+    /** @type {NestedWorkArray} */ ([])
+  );
+
+  return nestedWork
+    .map((work) => {
+      return options.fn(work);
+    })
+    .join('');
+}
