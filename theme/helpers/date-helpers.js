c5bd949f109ea7a9f5501b8dea3058d9f84a32dc
Edmund Rosewright <edmundrosewright@gmail.com>
2024-08-08 13:01:28 +0900

    fix: return type in jsdoc

--- theme/helpers/date-helpers.js
+++ theme/helpers/date-helpers.js
@@ -3,17 +3,17 @@ import moment from 'moment';
 export const dateHelpers = {
   /**
    * @param {Date} date
-   * @returns string
+   * @returns {string}
    */
   MY: (date) => moment(date.toString(), ['YYYY-MM-DD']).format('MMM YYYY'),
   /**
    * @param {Date} date
-   * @returns string
+   * @returns {string}
    */
   Y: (date) => moment(date.toString(), ['YYYY-MM-DD']).format('YYYY'),
   /**
    * @param {Date} date
-   * @returns string
+   * @returns {string}
    */
   DMY: (date) => moment(date.toString(), ['YYYY-MM-DD']).format('D MMM YYYY'),
 };
