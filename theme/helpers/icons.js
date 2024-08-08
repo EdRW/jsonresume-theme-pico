47d5145953f2f096573a450d2b8fae4d2015603a
Edmund Rosewright <edmundrosewright@gmail.com>
2024-08-08 13:00:48 +0900

    feat: add icons to about section

---
+++ theme/helpers/icons.js
@@ -0,0 +1,31 @@
+/**
+ * @typedef {import('simple-icons')} SimpleIcon
+ */
+
+import * as icons from 'simple-icons';
+import handlebars from 'handlebars';
+
+/**
+ * @param {string} name
+ * @returns {string | handlebars.SafeString}
+ */
+export function BrandIcon(name) {
+  const normalizedName = normalizeName(name);
+  const icon = icons[normalizedName];
+
+  if (!icon || !icon.svg) {
+    return name;
+  }
+
+  return new handlebars.SafeString(icon.svg);
+}
+
+/**
+ * @param {string} name
+ * @returns {keyof SimpleIcon}
+ */
+function normalizeName(name) {
+  name = name.toLowerCase().replace(' ', '');
+  const slug = name.charAt(0).toUpperCase() + name.slice(1);
+  return /** @type {keyof SimpleIcon} */ (`si${slug}`);
+}
--- theme/helpers/index.js
+++ theme/helpers/index.js
@@ -1,7 +1,9 @@
 import { dateHelpers } from './date-helpers.js';
 import { nestedWork } from './nested-work.js';
+import { BrandIcon } from './icons.js';
 
 export default {
   ...dateHelpers,
   nestedWork,
+  BrandIcon,
 };
